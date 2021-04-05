518d60c6b485185132c6fcae003af9d09d6a9f50
korshunovamm <korshunova.mm@phystech.edu>
2021-04-06 00:06:45 +0300

    allocated the cipher selection function to a separate class, added EsceptErrors function

--- main.py
+++ main.py
@@ -1,35 +1,23 @@
-from Encode_and_Decode_CesarClass import EncodeCesarClass, DecodeCesarClass
-from Encode_and_Decode_VigenereClass import EncodeVigenereClass, DecodeVigenereClass
-from Encode_and_Decode_VernamClass import CodeVernamClass
+from CreatorClientCipherClass import choices
 
 
-def choices(text):
-    """Функция возвращает класс кодирования/декодирования Цезаря/Виженера/Вернама.
-    Учитывается случай неправильного ввода данных"""
-
-    code_class = [EncodeCesarClass, EncodeVigenereClass, CodeVernamClass,
-                  DecodeCesarClass, DecodeVigenereClass, CodeVernamClass]
-
-    def excepterror(text):
-        try:
-            number = int(input("""Выберите способ обработки файла:
-                  зашифровать кодом Цезаря   -  0                   расшифровать кодом Цезаря   - 3
-                  зашифровать кодом Виженера -  1                   расшифровать кодом Виженера - 4
-                  зашифровать кодом Вернама  -  2                   расшифровать кодом Вернама  - 5
-                  """))
-            return code_class[number](text)
-        except IndexError:
-            excepterror(text)
-    return excepterror(text)
-
-
-# читаю текст файла
-path = input("Введите абсолютный путь к файлу: ")
-with open(path, encoding='utf-8', mode='r') as f:
-    TEXT = f.read()
-f.close()
+# читаю текст из файла, предусматривая ошибки неправильного ввода пути файла
+def except_path_file_errors():
+    try:
+        path = input("Введите абсолютный путь к файлу: ").strip()
+        with open(path, encoding='utf-8', mode='r') as f:
+            text = f.read()
+        f.close()
+        return text
+    except FileNotFoundError:
+        print("Нет такого файла!")
+        except_path_file_errors()
+    except IsADirectoryError:
+        print("Это каталог!")
+        except_path_file_errors()
 
 
 # вывод шифрования в консоль
-a = choices(TEXT).code()
+text = except_path_file_errors()
+a = choices(text).code()
 # choices(a).code()
