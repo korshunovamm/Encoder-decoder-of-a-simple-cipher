14434202a34160a6bceaf5e80b7277e6036eacc3
korshunovamm <korshunova.mm@phystech.edu>
2021-04-12 13:35:18 +0300

    added comments for reading file

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ from CreatorClientCipherClass import choices
 # читаю текст из файла, предусматривая ошибки неправильного ввода пути файла
 def except_path_file_errors():
     try:
+        # читаю абсолютный путь к файлу, убирая пробелы в начале и в конце
         path = input("Введите абсолютный путь к файлу: ").strip()
+        # открываю для чтения клиентский файл по формату utf-8 и записываю его содержимое в text
         with open(path, encoding='utf-8', mode='r') as f:
             text = f.read()
+        # закрываю файл
         f.close()
         return text
     except FileNotFoundError:
